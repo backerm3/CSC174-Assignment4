f11dd4f7ed46a1f08113327a93dbd4d62953f0dd
JackiePowell <31747169+JackiePowell@users.noreply.github.com>
2017-09-24 10:04:41 -0400

    Add files via upload

---
+++ jackiepowell.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Jackie Powell</title>
+</head>
+
+<body>
+	<h1>Jackie Powell</h1>
+	<em> Aspiring Social Justice Journalist and Sports Columnist</em>
+	
+	<h2>Projects:</h2>
+	<h3>Campus Times: Former Sports Editor, Current Columnist</h3>
+	<p>Served as the former sports editor where she created budgets, edited 
+	articles, carried out interviews and even wrote pieces for the section. 
+	Today she writes sports columns which correspond to her radio show about the social
+	 and political stories that collide with sports. </p>
+	
+	<h3>Harriet Harman MP: Constituency Office Intern</h3>
+	<p>Provided the consituency office with instant support. Wrote letters and
+	 spoke with constituents including those with immigration, housing and other issues 
+	 citizens couldn't get resolved by their local councils. Assisted the MP in helping 
+	 her promote her book "A Woman's Work". </p>
+	
+    <h3>Everytalks: Radio Talkshow Host</h3>
+	<p> Hosts a sports radio talk show every Friday on the University's online radio 
+		station, the Sting. The show features news updates, hot takes and interviews with 
+		guests, which all revolve around sports, feminism and the social issues that surround both.
+    </p>
+
+	<h3>Major League Baseball: Summer Intern</h3>
+	<p>Worked from MLB's International Department withing the marketing partnerships group. 
+	   Assisted in the creation of sponsorship decks and created deal memos for each individual 
+	   sponosor. Helped brainstorm the logistics for a fanfest event to be held in Canada next summer. </p>
+</body>
+
+</html>
