73f2f86b1c811a33cd3a79660cba46e21b694d50
backerm3 <backerm3@u.rochester.edu>
2017-09-25 00:36:19 -0400

    Add my (Ben's) bio and minor adjustments

--- index.php
+++ index.php
@@ -1,102 +1,83 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <meta  charset="utf-8">
-    
-
-    <title>Who We Are </title>
-   
+    <meta charset="utf-8">
+    <title>Who We Are</title>
 </head>
  
 <body>
     <header>
-        
-<section>
-
- <nav class="main-menu">
-    
-  
-    <ul>
-      <li class="current"> <a href="index.php">Home</a></li>
-      <li><a href="gregmarshall.php">Greg Marshall</a></li>
-      <li><a href="benackerman.php">Ben Ackerman</a></li>
-      <li><a href="jackiepowell.php">Jackie Powell</a></li>
-      
-    </ul> 
-
-   </nav>               
-
-</section> 
-
-
-
+        <section>
+            <nav class="main-menu">
+                <ul>
+                  <li class="current"><a href="index.php">Home</a></li>
+                  <li><a href="gregmarshall.php">Greg Marshall</a></li>
+                  <li><a href="benackerman.php">Ben Ackerman</a></li>
+                  <li><a href="jackiepowell.php">Jackie Powell</a></li>
+                </ul>
+            </nav>               
+        </section> 
     </header>
-    
 
-<div class= "container">
-    
-    <section class= "About US">
-        
-     <h1> About Us </h1>
-        
-        <div class= "minibios">
-            <div class="bio1">
-                <h2>Greg Marshall</h2>
-            <p> "Greg Marshall (often referred to as Greg The Great for 
-          his genius and winning personality) was born in Syracuse, New York. He 
-          lived out the majority of his childhood there before graduating from 
-          high school at Jordan-Elbridge High School. He decided he would attend 
-          the University of Rochester to pursue a degree in Biology, a science 
-          which he maintains a love for today, but found that it was not a field 
-          he wished to develop a career in. Pivoting substantially, he entered a 
-          business program at Rochester's Simon School of Business. It was also 
-          around this time he chose to tryout for the Men's rowing team. 
-          It represented a transition from years of running, so the significant 
-          cardovascular nature of rowing appealled to him. 
-          He continues his pursuit of a B.S. in business today, and his love for 
-          his team and the sport of rowing grows with every early morning practice." </p>
-                
-            <a href= "gregmarshall.php"> Click for more about Greg </a>
-            </div>   
-            
-            <div class= "bio2">
-                <h2>Ben Ackerman</h2>
-                <p> "" </p>
-                <a href= "benackerman.php"> Click for more about Ben </a>
-            </div>   
+    <div class="container">
+        <section class="about_us">
+            <h1>About Us</h1>
+            <div class="minibios">
+                <div class="bio1">
+                    <h2>Greg Marshall</h2>
+                    <p>"Greg Marshall (often referred to as Greg The Great for 
+                      his genius and winning personality) was born in Syracuse, New York. He 
+                      lived out the majority of his childhood there before graduating from 
+                      high school at Jordan-Elbridge High School. He decided he would attend 
+                      the University of Rochester to pursue a degree in Biology, a science 
+                      which he maintains a love for today, but found that it was not a field 
+                      he wished to develop a career in. Pivoting substantially, he entered a 
+                      business program at Rochester's Simon School of Business. It was also 
+                      around this time he chose to tryout for the Men's rowing team. 
+                      It represented a transition from years of running, so the significant 
+                      cardiovascular nature of rowing appealled to him. 
+                      He continues his pursuit of a B.S. in business today, and his love for 
+                      his team and the sport of rowing grows with every early morning practice."</p>
+                    <a href= "gregmarshall.php">Click for more about Greg</a>
+                </div>   
 
-           
-             <div class="bio3">
-                <h2>Jackie Powell</h2>
-            <p> "Jackie Powell is a current International Relations major while 
-            also pursuing a journalism minor at the University of Rochester. 
-            She is passionate about instagating change and has recently been 
-            doing that through her work at the Campus Times trying to help 
-            expose wrongdoings within the school adminstration, while also 
-            writing a sports column. She is an avid sports fan and a staunch 
-            feminist. She has a Sports radio talk show where she discusses the 
-            most prevelant social issues affecting the world of sports.
-            She also likes to educate her listneers by honoring a Woman of the Week. 
-            Aside from all of her writing and communicating, Jackie loves singing, 
-            espcially in UR's No Jackets Required." </p>
-                <a href= "jackiepowell.php"> Click for more about Jackie </a>
-            </div>  
+                <div class="bio2">
+                    <h2>Ben Ackerman</h2>
+                    <p>"Ben Ackerman is a Computer Science student and aspiring broadcast engineer
+                        at the University of Rochester.  Originally from Fairfax, VA, he got into
+                        computers at a very young age, and has been interested in all things tech
+                        ever since.  In addition to studying at UR, Ben also works in the engineering
+                        department at WXXI Public Broadcasting in Rochester.  There, he is known
+                        for having written a program that allowed WXXI to transmit show and song
+                        information to text-enabled radios.  Along the side, Ben also helps out at
+                        WRUR, both in engineering and by co-hosting a classic rock show, and at the
+                        Computer Interest Floor (CIF) as their tech maintenance director, maintaining
+                        their computer lab in the basement of Anderson Tower."</p>
+                    <a href="benackerman.php">Click for more about Ben</a>
+                </div>   
 
-        </div>    
+                <div class="bio3">
+                    <h2>Jackie Powell</h2>
+                    <p>"Jackie Powell is a current International Relations major while 
+                    also pursuing a journalism minor at the University of Rochester. 
+                    She is passionate about instigating change and has recently been 
+                    doing that through her work at the Campus Times trying to help 
+                    expose wrongdoings within the school adminstration, while also 
+                    writing a sports column. She is an avid sports fan and a staunch 
+                    feminist. She has a Sports radio talk show where she discusses the 
+                    most prevelant social issues affecting the world of sports.
+                    She also likes to educate her listneers by honoring a Woman of the Week. 
+                    Aside from all of her writing and communicating, Jackie loves singing, 
+                    espcially in UR's No Jackets Required."</p>
+                    <a href="jackiepowell.php">Click for more about Jackie</a>
+                </div>  
+            </div>    
+        </section>
+    </div>             
 
-    </section>
-
-</div>             
-
-
-<footer>
-
-    <footer class= "footer">
-    
-    <p> CSC 174 Webpage Design and Development </p>
-
-    <p> Created by Marshall, Ackerman and Powell </P>    
-        
+    <footer class="footer">
+    <p>CSC 174 Webpage Design and Development</p>
+    <p>Created by Marshall, Ackerman and Powell</p>    
     </footer>
     
 </body>
