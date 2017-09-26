dee9c624f4501df41320c19484742cbc1c23f70c
JackiePowell <31747169+JackiePowell@users.noreply.github.com>
2017-09-25 21:18:07 -0400

    Update jackiepowell.php

--- jackiepowell.php
+++ jackiepowell.php
@@ -10,6 +10,18 @@
 	<h1>Jackie Powell</h1>
 	<em> Aspiring Social Justice Journalist and Sports Columnist</em>
 	
+	<p>"Jackie Powell is a current International Relations major while 
+            also pursuing a journalism minor at the University of Rochester. 
+            She is passionate about instigating change and has recently been 
+            doing that through her work at the Campus Times trying to help 
+            expose wrongdoings within the school adminstration, while also 
+            writing a sports column. She is an avid sports fan and a staunch 
+            feminist. She has a Sports radio talk show where she discusses the 
+            most prevelant social issues affecting the world of sports.
+            She also likes to educate her listneers by honoring a Woman of the Week. 
+            Aside from all of her writing and communicating, Jackie loves singing, 
+            espcially in UR's No Jackets Required."</p>
+	
 	<h2>Projects:</h2>
 	<h3>Campus Times: Former Sports Editor, Current Columnist</h3>
 	<p>Served as the former sports editor where she created budgets, edited 
